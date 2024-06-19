f7c243295d2d2b659d24e827f9c3e48f27487f80
Miguel Garcia <miguelfiuza315@gmail.com>
2024-06-18 21:31:30 -0300

    alterações

--- src/Gato.java
+++ src/Gato.java
@@ -1,2 +1,20 @@
-public class Gato {
+public class Gato extends Felino {
+    private String raca;
+
+    public Gato(String nome, int idade, boolean temPelos, boolean gostaDeEscalar, String raca) {
+        super(nome, idade, temPelos, gostaDeEscalar);
+        this.raca = raca;
+    }
+
+    public void miar() {
+        System.out.println("O gato está miando.");
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
 }
--- src/Zoologico.java
+++ src/Zoologico.java
@@ -1,12 +1,14 @@
 public class Zoologico {
     public static void main(String[] args) {
-        Canino cachorro = new Canino("Bob", 5, true, "Au Au");
-        cachorro.emitirSom();
-        cachorro.latir();
+        Lobo lobo = new Lobo("Lobo Cinzento", 6, true, "Auuuuu", "Floresta");
+        lobo.emitirSom();
+        lobo.latir();
+        lobo.uivar();
 
-        Felino gato = new Felino("Garfield", 3, true, true);
+        Gato gato = new Gato("Mimi", 3, true, true, "Persa");
         gato.emitirSom();
         gato.escalar();
+        gato.miar();
 
         Papagaio papagaio = new Papagaio("Louro", 2, true, "Verde");
         papagaio.emitirSom();
